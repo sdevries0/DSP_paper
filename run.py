7a110a26f924c4504c009c68eab30f0ace6f6fd3
sdevries0 <sigur.devries@ru.nl>
2024-12-02 12:47:14 +0100

    add variables

--- run.py
+++ run.py
@@ -85,7 +85,7 @@ def run(env_string, algorithm_string):
     if algorithm_string == "FF":
         fitness_function = ff_evaluate.Evaluator(env, 0.01, stepsize_controller=diffrax.PIDController(atol=1e-6, rtol=1e-6, dtmin=0.001), max_steps=500)
 
-        variable_list = [["y" + str(i) for i in range(env.n_obs)], ["a" + str(i) for i in range(state_size)] + ["u"] + ["tar" + str(i) for i in range(env.n_control)]]
+        variable_list = [["y" + str(i) for i in range(env.n_obs)], ["a" + str(i) for i in range(state_size)] + ["u"] + ["tar" + str(i) for i in range(env.n_targets)]]
 
         layer_sizes = jnp.array([env.n_control])
 
@@ -96,7 +96,6 @@ def run(env_string, algorithm_string):
         fitness_function = dynamic_evaluate.Evaluator(env, state_size, 0.05, solver=diffrax.Dopri5(), stepsize_controller=diffrax.PIDController(atol=1e-4, rtol=1e-4, dtmin=0.001), max_steps=1000)
 
         variable_list = [["y" + str(i) for i in range(env.n_obs)] + ["a" + str(i) for i in range(state_size)] + ["u"]]
-
 
         layer_sizes = jnp.array([state_size, env.n_control])
         

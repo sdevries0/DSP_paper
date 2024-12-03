03ab651dcde5912f16bbc0d329690b9bf0892739
sdevries0 <sigur.devries@ru.nl>
2024-12-03 14:42:59 +0100

    wrote scripts to run all experiments

---
+++ CMAES.py
@@ -0,0 +1,96 @@
+import jax
+from jax import Array
+import jax.numpy as jnp
+from jax.random import PRNGKey
+import evosax
+
+from typing import Tuple
+
+class CMA_ES():
+    """Covariance matrix adaptation evolution strategy of symbolic expressions.
+
+        Attributes:
+            num_generations: The number of generations over which to evolve the population.
+            population_size: Number of candidates in the population.
+            fitness_function: Function that evaluates a candidate and assigns a fitness.
+            current_generation: A reference to keep track of the current generation.
+            best_solutions: Best solution at each generation.
+            best_fitnesses: Best fitness at each generation.
+            cma_strategy: Evosax implementation of CMA-ES.
+            cma_state: State that controls the evolution of CMA-ES.
+
+        """
+    def __init__(self, num_generations, population_size, fitness_function, num_dims, key) -> None:
+        self.cma_strategy = evosax.CMA_ES(popsize = population_size, num_dims = num_dims, elite_ratio=0.1, sigma_init=0.1)
+        self.cma_state = self.cma_strategy.initialize(key)
+        self.current_generation = 0
+        self.fitness_function = fitness_function
+        self.num_generations = num_generations
+        self.best_fitnesses = jnp.zeros(self.num_generations)
+        self.best_solutions = jnp.zeros((self.num_generations, num_dims))
+
+    def initialize_population(self, key: PRNGKey) -> list:
+        """Randomly initializes the population.
+
+        :param key: Random key
+
+        Returns: Population.
+        """
+        population, self.cma_state = self.cma_strategy.ask(key, self.cma_state)
+        return population
+
+    def evaluate_population(self, population: Array, data: Tuple) -> Tuple[Array, list]:
+        """Evaluates every candidate in population and assigns a fitness.
+
+        :param population: Population of candidates
+        :param data: The data required to evaluate the population.
+
+        Returns: Fitness and evaluated population.
+        """
+        fitnesses = jax.vmap(self.fitness_function, in_axes=[0,None])(population, data)
+        self.cma_state = self.cma_strategy.tell(population, fitnesses, self.cma_state)
+
+        best_idx = jnp.argmin(fitnesses)
+        best_fitness_of_g = fitnesses[best_idx]
+        best_solution_of_g = population[best_idx]
+
+        #Keep track of best solution
+        if self.current_generation == 0:
+            best_fitness = best_fitness_of_g
+            best_solution = best_solution_of_g
+        else:
+            best_fitness = self.best_fitnesses[self.current_generation - 1]
+            best_solution = self.best_solutions[self.current_generation - 1]
+
+            if best_fitness_of_g < best_fitness:
+                best_fitness = best_fitness_of_g
+                best_solution = best_solution_of_g
+
+        self.best_fitnesses = self.best_fitnesses.at[self.current_generation].set(best_fitness)
+        self.best_solutions = self.best_solutions.at[self.current_generation].set(best_solution)
+
+        return fitnesses, population
+
+    def evolve(self, population: Array, fitness: Array, key: PRNGKey) -> Array:
+        """Creates a new population by evolving the current population.
+
+        :param population: Population of candidates
+        :param key: Random key.
+
+        Returns: Population with new candidates.
+        """  
+        population, self.cma_state = self.cma_strategy.ask(key, self.cma_state)
+        self.current_generation += 1
+        return population
+    
+    def get_statistics(self, generation: int = None) -> Tuple[Array | int, Array]:
+        """Returns best fitness and best solution.
+
+        :param generation: Generation of which the best fitness and solution are required. If None, returns all best fitness and solutions.
+
+        Returns: Best fitness and best solution.
+        """
+        if generation is not None:
+            return self.best_fitnesses[generation]
+        else:
+            return self.best_fitnesses
--- run.py
+++ run.py
@@ -7,25 +7,20 @@ sys.path.append("/Users/sigur.de.vries/Library/Mobile Documents/com~apple~CloudD
 import jax
 import jax.numpy as jnp
 import jax.random as jrandom
-import matplotlib.pyplot as plt
-import sympy
-import diffrax
-
 
 import MultiTreeGP.evaluators.dynamic_evaluate as dynamic_evaluate
-import MultiTreeGP.evaluators.feedforward_evaluate as ff_evaluate
-import MultiTreeGP.evaluators.lqg_evaluate as lqg_evaluate
+import MultiTreeGP.evaluators.static_evaluate as static_evaluate
+import CMAES_evaluate
+import LQG_evaluate
+from NDE import ParameterReshaper
 
 from MultiTreeGP.genetic_programming import GeneticProgramming
+from CMAES import CMA_ES
 
-from MultiTreeGP.environments.control_environments.harmonic_oscillator import HarmonicOscillator, HarmonicOscillator2, ChangingHarmonicOscillator
+from MultiTreeGP.environments.control_environments.harmonic_oscillator import HarmonicOscillator, ChangingHarmonicOscillator
 from MultiTreeGP.environments.control_environments.reactor import StirredTankReactor
-from MultiTreeGP.environments.control_environments.cart_pole import CartPole
 from MultiTreeGP.environments.control_environments.acrobot import Acrobot, Acrobot2
-
-def my_to_string(sol):
-    expr = sympy.parsing.sympy_parser.parse_expr(sol,evaluate=True)
-    return simplification.sympy_to_tree(expr,mode="Add")
+import diffrax
 
 def get_data(key, env, batch_size, dt, T, param_setting):
     init_key, noise_key1, noise_key2, param_key = jrandom.split(key, 4)
@@ -38,83 +33,128 @@ def get_data(key, env, batch_size, dt, T, param_setting):
     return x0, ts, targets, process_noise_keys, obs_noise_keys, params
 
 
-key = jrandom.PRNGKey(0)
-init_key, data_key = jrandom.split(key)
+def run(env_string, algorithm_string, seed = 0, param_setting = "Constant"):
+    key = jrandom.PRNGKey(seed)
+    init_key, data_key = jrandom.split(key)
 
-population_size = 100
-num_populations = 3
-num_generations = 50
-state_size = 2
-T = 50
-dt = 0.2
-batch_size = 8
+    population_size = 100
+    num_populations = 5
+    num_generations = 50
+    state_size = 2
+    T = 50
+    dt = 0.2
+    batch_size = 8
 
-process_noise = 0.05
-obs_noise = 0.1
-
-param_setting = "Constant"
-
-
-def run(env_string, algorithm_string):
+    process_noise = 0.05
+    obs_noise = 0.1
 
     if env_string=="HO":
-        env = HarmonicOscillator(process_noise, obs_noise, n_obs=2)
-        # env = ChangingHarmonicOscillator(process_noise, obs_noise, n_obs=2)
+        if param_setting == "Changing":
+            env = ChangingHarmonicOscillator(process_noise, obs_noise, n_obs=2)
+        else:
+            env = HarmonicOscillator(process_noise, obs_noise, n_obs=1)
+
+        operator_list = [("+", lambda x, y: x + y, 2, 0.5), 
+                    ("-", lambda x, y: x - y, 2, 0.1),
+                    ("*", lambda x, y: x * y, 2, 0.5),
+                    ("/", lambda x, y: x / y, 2, 0.1),
+                    ("**", lambda x, y: x ** y, 2, 0.1),
+                    ]
 
     elif env_string=="ACR":
         env = Acrobot(process_noise, obs_noise)
 
         # env = Acrobot2(process_noise, obs_noise, n_obs=None)
 
+        operator_list = [("+", lambda x, y: x + y, 2, 0.5), 
+                        ("-", lambda x, y: x - y, 2, 0.1),
+                        ("*", lambda x, y: x * y, 2, 0.5),
+                        ("/", lambda x, y: x / y, 2, 0.1),
+                        ("**", lambda x, y: x ** y, 2, 0.1),
+                        ("sin", lambda x: jnp.sin(x), 1, 0.1),
+                        ("cos", lambda x: jnp.cos(x), 1, 0.1)
+                        ]
+
     elif env_string=="CSTR":
         env = StirredTankReactor(process_noise, obs_noise, n_obs=2)
 
-    operator_list = [("+", lambda x, y: x + y, 2, 0.5), 
-                    ("-", lambda x, y: x - y, 2, 0.1),
-                    ("*", lambda x, y: x * y, 2, 0.5),
-                    # ("/", lambda x, y: x / y, 2, 0.1),
-                #  ("**", lambda x, y: x ** y, 2, 0.1),
-                ("sin", lambda x: jnp.sin(x), 1, 0.1),
-                ("cos", lambda x: jnp.cos(x), 1, 0.1)
-                # ("exp", lambda x: jnp.exp(x), 1, 0.1),
-                #  ("log", lambda x: jnp.log(x), 1, 0.1)
-                    ]
+        operator_list = [("+", lambda x, y: x + y, 2, 0.5), 
+                        ("-", lambda x, y: x - y, 2, 0.1),
+                        ("*", lambda x, y: x * y, 2, 0.5),
+                        ("/", lambda x, y: x / y, 2, 0.1),
+                        ("**", lambda x, y: x ** y, 2, 0.1),
+                        ("exp", lambda x: jnp.exp(x), 1, 0.1),
+                        ("log", lambda x: jnp.log(x), 1, 0.1)
+                        ]
 
     data = get_data(data_key, env, batch_size, dt, T, param_setting)
 
-    if algorithm_string == "FF":
-        fitness_function = ff_evaluate.Evaluator(env, 0.01, stepsize_controller=diffrax.PIDController(atol=1e-6, rtol=1e-6, dtmin=0.001), max_steps=500)
+    if algorithm_string == "Static":
+        fitness_function = static_evaluate.Evaluator(env, 0.01, max_steps = 10000, solver=diffrax.GeneralShARK())
 
-        variable_list = [["y" + str(i) for i in range(env.n_obs)], ["a" + str(i) for i in range(state_size)] + ["u"] + ["tar" + str(i) for i in range(env.n_targets)]]
+        variable_list = [["y" + str(i) for i in range(env.n_obs)] + ["tar" + str(i) for i in range(env.n_targets)]]
 
         layer_sizes = jnp.array([env.n_control])
 
         strategy = GeneticProgramming(num_generations, population_size, fitness_function, operator_list, variable_list, layer_sizes,
-                                num_populations = num_populations, coefficient_sd=3, max_depth=8, max_nodes=25, max_init_depth=4)
+                                num_populations = num_populations)
 
-    elif algorithm_string == "GP":
-        fitness_function = dynamic_evaluate.Evaluator(env, state_size, 0.05, solver=diffrax.Dopri5(), stepsize_controller=diffrax.PIDController(atol=1e-4, rtol=1e-4, dtmin=0.001), max_steps=1000)
+    elif algorithm_string == "Dynamic" or algorithm_string == "Random":
+        fitness_function = dynamic_evaluate.Evaluator(env, state_size, 0.01, max_steps = 10000, solver=diffrax.GeneralShARK())
 
-        variable_list = [["y" + str(i) for i in range(env.n_obs)] + ["a" + str(i) for i in range(state_size)] + ["u"]]
+        variable_list = [["y" + str(i) for i in range(env.n_obs)] + ["a" + str(i) for i in range(state_size)] + ["u"] + ["tar" + str(i) for i in range(env.n_targets)],
+                         ["a" + str(i) for i in range(state_size)]]#  + ["tar" + str(i) for i in range(env.n_targets)]]
 
         layer_sizes = jnp.array([state_size, env.n_control])
         
         strategy = GeneticProgramming(num_generations, population_size, fitness_function, operator_list, variable_list, layer_sizes, 
-                                num_populations = num_populations, coefficient_sd=1, max_init_depth=4, max_nodes=25, coefficient_optimisation=True, gradient_steps=15)
+                                num_populations = num_populations)
+        
+    elif algorithm_string == "NDE":
+        latent_dim = 16
+        parameter_reshaper = ParameterReshaper(env.n_obs + env.n_control, latent_dim, env.n_control, env.n_targets)
+        fitness_function = CMAES_evaluate.Evaluator(env, parameter_reshaper, latent_dim, 0.01, max_steps = 10000, solver=diffrax.GeneralShARK())
+
+        key, cma_key = jrandom.split(key)
+        strategy = CMA_ES(num_generations, population_size*num_populations, fitness_function, parameter_reshaper.total_parameters, cma_key)
 
     elif algorithm_string == "LQG":
-        fitness_function = lqg_evaluate.Evaluator(env)
+        fitness_function = LQG_evaluate.Evaluator(env, 0.01)
         _,_,_, fitness = fitness_function(data)
+
+        print(fitness)
+
+        return fitness
 
     population = strategy.initialize_population(init_key)
 
     for g in range(num_generations):
         fitness, population = strategy.evaluate_population(population, data)
-        best_fitness, best_solution = strategy.get_statistics(g)
-        print(f"In generation {g+1}, best fitness = {best_fitness:.4f}, best solution = {strategy.to_string(best_solution)}")
+        
+        if algorithm_string in ["Static", "Dynamic", "Random"]:
+            best_fitness, best_solution = strategy.get_statistics(g)
+            print(f"In generation {g+1}, best fitness = {best_fitness:.4f}, best solution = {strategy.to_string(best_solution)}")
+        else:
+            best_fitness = strategy.get_statistics(g)
+            print(f"In generation {g+1}, best fitness = {best_fitness:.4f}")
 
         if g < (num_generations-1):
             key, sample_key = jrandom.split(key)
-            population = strategy.evolve(population, fitness, sample_key)
+            if algorithm_string == "Random":
+                population = strategy.initialize_population(sample_key)
+                population[0,0] = best_solution
+                strategy.increase_generation()
+            else:
+                population = strategy.evolve(population, fitness, sample_key)
 
-    best_fitnesses, best_solutions = strategy.get_statistics()
+    return strategy.get_statistics(g)
+
+param_settings = ["Constant", "Different", "Changing"]
+envs = ["HO", "ACR", "CSTR"]
+algorithms = ["Static", "Dynamic", "Random", "NDE", "LQG"]
+
+best_fitness, best_solutions = run("HO", "Static", "Constant")
+# best_fitness, best_solutions = run("HO", "Dynamic", "Constant")
+# best_fitness, best_solutions = run("HO", "Random", "Constant")
+# best_fitness = run("HO", "NDE", "Constant")
+# fitness = run("HO", "LQG", "Constant")
